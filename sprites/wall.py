6883a0243c0a694d0f4b2e8e1f6a08c41c7ea961
Ronin-The-Barbarian <roningambill@outlook.com>
2024-12-09 10:04:09 -0600

    Revert "Testing Branch"
    This reverts commit d3892633a9c19680448c71a3b312761f06921bfe.

--- sprites/wall.py
+++ sprites/wall.py
@@ -12,7 +12,6 @@ class Wall(pygame.sprite.Sprite):
         self.speed = Kspeed
         self.actualX = pos[0]
         self.actualY = pos[1]
-        # TEST
 
     # Move off screen
     def scroll(self, direction,speed):
