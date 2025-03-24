e0666f38506b6f49d14cd71696645b5987ab2131
Ronin-the-Barbarian <roningambill@outlook.com>
2025-03-23 20:10:51 -0500

    TSA Missouri State Championship

--- sprites/music.py
+++ sprites/music.py
@@ -7,9 +7,10 @@ class Music(pygame.sprite.Sprite):
         super().__init__()
         # Load image and rect
         self.music = music
-        self.volume = 0
+        self.volume = 0.2
     def load(self,looping=-1):
         pygame.mixer.init()
+        pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.load("music/" + self.music + ".ogg")
         pygame.mixer.music.play(looping)
     def unload(self):
